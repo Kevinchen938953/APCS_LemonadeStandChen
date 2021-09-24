efed154d30a4b977b95649c2cea70d1c88d4fdf8
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2021-09-24 09:20:51 -0400

    Update LemonadeStand.java

--- LemonadeStand.java
+++ LemonadeStand.java
@@ -1,69 +1,93 @@
-		//Variables - done
-		/*Methods?
-		makeLemonade?
-		sellLemonade?
-		shrink? lose an item  - ice melts, lemons rot, 
-		dailyBatch?
-		*/
-public class LemonadeStand {
-
-		int numLemons;
-		int cupsSugar;
-		int cupsIce;
-		int numCups;		//num paper cups
-		int cupsMade=0;
-		int numPitchers=0;
-		int numCustomers;  //
-		double pricePerCup;
-		double weatherScore; //think about
-
-		//CONSTRUCTOR
-		public LemonadeStand() {
-			numLemons = 9;
-			cupsSugar = 9;
-			cupsIce=9;
-			
-		}
-
-
-
-		public void makeLemonade() {  //DEFAULT METHOD
-			//recipe? 3 numLemons + 3 cupsSugar + 3 cupsIce
-			//to make a pitcher, add 1 to the count of num pitcher
-			if (numLemons>=3 && cupsSugar>=3 && cupsIce >=3) {
-				numPitchers++;
-				numLemons-=3;  //numLemons = numLemons - 3
-				cupsSugar-=3;
-				cupsIce-=3;
-				System.out.println("\nMade a pitcher of lemonade\n");
-				
-			}
-		}
-		/* OVERLOADED METHOD - a 'custom method' passing in arguments ** */										
-		public void makeLemonade(int nL, int cS, int cI) {
-			//recipe? 3 numLemons + 3 cupsSugar + 3 cupsIce
-			//to make a pitcher, add 1 to the count of num pitcher
-			if (numLemons>=nL && cupsSugar>=cS && cupsIce >=cI) {
-				numPitchers++;
-				numLemons-=nL;  //numLemons = numLemons - 3
-				cupsSugar-=cS;
-				cupsIce-=cI;
-			}
-		}
-		
-		public void sellLemonade() {
-			//  ? how many pitchers did I make?
-			// how many cups in a pitcher
-			//how many cups total available to pour??
-			
-			cupsMade++;
-			//print out num cups && pitchers left??
-		}
-		
+		import java.util.Scanner;
+public class Lemonade
+{
+	public static void main(String[]args)
+	{
 	
 		
+		//Methods selllemons makeLemons dailyBatch?
+		Scanner getdays = new Scanner(System.in);
+		System.out.print("How many days? 7, 14, or 21?");
+		int day = getdays.nextInt(); 
+		while (day!=7 && day!=14 && day!= 21)
+		{
+			System.out.println("Select again: ");
+			day = getdays.nextInt(); 
+			
+		}
+		System.out.print("You chose this" + " " + day);
 		
+	
+    
+    public void buystuff(String ingredients, String item)
+    {
 		
+		System.out.print("What do you want to buy or do you want to exit?");
+		Scanner buythis = new Scanner(System.in);
+		Scanner amount = new Scanner(System.in);
+		String ingredients = buythis.nextline();
 		
+		while((!ingredients.equals("Exit") || !ingredients.equals("exit")) && money>=0 )
+		{
+			System.out.print("Do you want to buy Lemon, Sugar, Ice, or Cups?");
+			Scanner chooseitem = new Scanner(System.in);
+			String item = Scanner.nextLine();
+			if(item.equals("Lemon"))  
+			{
+				System.out.println("How many lemons 10, 30, or 75?"); 
+				int lemonpiece = amount.nextInt() 
+				numLemons += lemonpiece;
+				money -= lemonPrice* lemonpiece;
+				
+			
+		    }
+		    else if(item.equals("Ice"))
+		    {
+				System.out.println("How many cups of Ice do you want to buy?");
+				int icepiece = amount.nextInt();
+				numIce += icepiece;
+				money -=icepiece*icePrice;
+			}
+			else if(item.equals("Sugar"))
+			{
+				System.out.println("How many cups of sugar do you want to buy?");
+				int sugarpiece = amount.nextInt();
+				numSugar += sugarpiece;
+				money -=sugarpiece*sugarPrice;
+		    }	
+	    }
+	
+	//public void makeLemonade(int nL, int cS, int cI)
+	//{
+		//if(numLemons>=3 && cupsSugar>=3 && cupsIce>=3)
+		//{
+			//numPitchers++;
+			//cupsMade+=10;
+			//n//umLemons-=3;
+			//cupsSugar-=3;
+			//cupsIce-=3;
+			//System.out.println("Made Lemonade");
+		//	
+	//	}
 		
-}
+	//}
+	
+	
+	//public void makeLemonade(int nL, int cS, int cI)
+	//{
+		//if(numLemons>=nL && cupsSugar>=cS && cupsIce>=cI)
+	//	{
+		//	numPitchers++;
+		//	cupsMade+=3;
+			//numLemons-=nL;
+		//	cupsSugar-=cS;
+		//	cupsIce-=cI;
+		//	System.out.println("Made Lemonade");
+			
+		//}
+		
+	//}
+	
+	
+	
+	
